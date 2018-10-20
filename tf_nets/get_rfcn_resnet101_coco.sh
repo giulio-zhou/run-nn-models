36fb7f94c5befa423871ea81985941fbc5e2c439
Giulio Zhou <giuliozhou8@gmail.com>
2018-10-20 16:18:24 +0000

    Scripts to get RFCN, FPN MobileNet and FPN ResNet

---
+++ tf_nets/get_rfcn_resnet101_coco.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+CWD=`pwd`
+DIR=`basename $CWD`
+
+prog() {
+    cd tf_nets
+
+    wget http://download.tensorflow.org/models/object_detection/rfcn_resnet101_coco_2018_01_28.tar.gz
+    tar -xvf rfcn_resnet101_coco_2018_01_28.tar.gz
+
+    rm rfcn_resnet101_coco_2018_01_28.tar.gz
+    mv rfcn_resnet101_coco_2018_01_28 rfcn_resnet101_coco
+
+    cd $CWD
+
+    if [ ! -d utils ]; then
+        mkdir utils
+    fi
+    if [ ! -f utils/__init__.py ]; then
+        touch utils/__init__.py
+    fi
+    cp models/research/object_detection/utils/visualization_utils.py utils
+}
+
+if [[ "$DIR" != "run_nn_models" ]] && [[ "$1" != "-f" ]];
+then
+    echo "Warning: you must run this script from the run_nn_models repo root, and I don't think you are."
+    echo "Run this again with -f if you're sure."
+else
+    prog
+fi
